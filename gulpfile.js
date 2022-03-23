5ca52f42c8a2687c2ac4d446c143a3916487e894
Inoki <veyx.shaw@gmail.com>
2022-03-23 19:13:41 +0100

    Fix build by ignoring minimization of js and css

--- gulpfile.js
+++ gulpfile.js
@@ -36,11 +36,11 @@ gulp.task('html', ['styles'], function () {
 
   return gulp.src('app/*.html')
     .pipe(assets)
-    .pipe($.if('*.js', $.uglify()))
-    .pipe($.if('*.css', $.csso()))
+    // .pipe($.if('*.js', $.uglify()))
+    // .pipe($.if('*.css', $.csso()))
     .pipe(assets.restore())
     .pipe($.useref())
-    .pipe($.if('*.html', $.minifyHtml({conditionals: true, loose: true})))
+    // .pipe($.if('*.html', $.minifyHtml({conditionals: true, loose: true})))
     .pipe(gulp.dest('dist'));
 });
 
